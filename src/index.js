ff104de9caad3735971de432510087a8eb20e080
CodeMD96 <codemd96@gmail.com>
2021-12-14 10:37:44 +0000

    adding fs functinality

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 const yargs = require("yargs");
-const Movie = require("./utils").Movie;
+const {Movie, pullMovieData} = require("./utils");
 const addActors = require("./utils/addActors.js");
-// below is from previous version, just showing how to export multiple things
-// const movieArr = require("./utils").movieArr;
 const args = yargs.argv;
 
 
 const app = () => {
+    pullMovieData();
     switch (args.command) {
         case "add":
             const newMovie = new Movie(args.title, addActors(args.actors));
             newMovie.add();
             newMovie.log();
+            newMovie.save();
             break;
         case "addMulti":
             const newMovie1 = new Movie(args.title1, addActors(args.actors1));
             newMovie1.add();
+            newMovie1.save();
             const newMovie2 = new Movie(args.title2, addActors(args.actors2));
             newMovie2.add();
             newMovie2.log();
+            newMovie2.save();
             break;
         default:
             console.log("Incorrect command");
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,13 @@
-const movieArr = [];
+const fs = require("fs")
+let movieArr;
 
+const pullMovieData = () => {
+    try {
+        movieArr = JSON.parse(fs.readFileSync("./storage.json"))
+    } catch (error) {
+        movieArr = []
+    }
+}
 class Movie {
     constructor(title, actors = "Not specified") {
         this.title = title;
@@ -12,7 +20,10 @@ class Movie {
     log() {
         console.log(movieArr);
     }
+
+    save() {
+        fs.writeFileSync("./storage.json", JSON.stringify(movieArr));
+    }
 }
 
-module.exports.Movie = Movie;
-module.exports.movieArr = movieArr;
+module.exports = {Movie, pullMovieData};
